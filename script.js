6c477dff5c210d34d0fdaadf614f36aaa2bb65a5
Thiago Mauricio Borges <tmb050703@gmail.com>
2025-02-14 17:34:38 -0300

    Initial commit

---
+++ script.js
@@ -0,0 +1,57 @@
+'use strict';
+
+// console.log(document.querySelector('.message').textContent);
+// document.querySelector('.message').textContent = '🎉 Correct Number! ';
+
+// document.querySelector('.number').textContent = 13;
+// document.querySelector('.score').textContent = 10;
+
+// document.querySelector('.guess').value = 23;
+// console.log(document.querySelector('.guess').value);
+
+
+
+const secretNumber = Math.trunc(Math.random()*20)+1;
+let score = 20;
+// document.querySelector('.number').textContent = secretNumber;
+
+document.querySelector('.check').addEventListener('click', function() {
+  const guess =  Number(document.querySelector('.guess').value);
+  console.log(guess, typeof guess);
+
+  // When there is no input
+  if(!guess) {
+    document.querySelector('.message').textContent = '⛔ No Number!';
+
+    // When player wins
+  } else if (guess === secretNumber) {
+    document.querySelector('.message').textContent = '🎉 Correct Number! ';
+    document.querySelector('.number').textContent = secretNumber;
+    document.querySelector('body').style.backgroundColor = '#60b347';
+    document.querySelector('.number').style.width = '30rem';
+
+    // When guss is too high
+  } else if (guess > secretNumber) {
+    if(score > 1) {
+      document.querySelector('.message').textContent = '📈 Too high!';
+      score--;
+      document.querySelector('.score').textContent = score;
+    } else {
+      document.querySelector('.message').textContent = '☠️ You lost the Game!';
+      document.querySelector('.score').textContent = 0;
+    }
+
+    // When is too low
+  } else if (guess < secretNumber) {
+    if(score > 1) {
+      document.querySelector('.message').textContent = '📉 Too low!';
+      score--;
+      document.querySelector('.score').textContent = score;
+    } else {
+      document.querySelector('.message').textContent = '☠️ You lost the Game!';
+      document.querySelector('.score').textContent = 0;
+    }
+  }
+});
+
+
