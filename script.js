9f9f4660c183130736d9f9c4e5e02dc7e56282b8
Thiago Mauricio Borges <tmb050703@gmail.com>
2025-02-16 13:29:42 -0300

    highScore add

--- script.js
+++ script.js
@@ -11,9 +11,10 @@
 
 
 
-const secretNumber = Math.trunc(Math.random()*20)+1;
+let secretNumber = Math.trunc(Math.random()*20)+1;
 let score = 20;
-// document.querySelector('.number').textContent = secretNumber;
+let highscore = 0;
+document.querySelector('.number').textContent = secretNumber;
 
 document.querySelector('.check').addEventListener('click', function() {
   const guess =  Number(document.querySelector('.guess').value);
@@ -30,7 +31,12 @@ document.querySelector('.check').addEventListener('click', function() {
     document.querySelector('body').style.backgroundColor = '#60b347';
     document.querySelector('.number').style.width = '30rem';
 
-    // When guss is too high
+    if(score > highscore) {
+      highscore = score;
+      document.querySelector('.highscore').textContent = highscore;
+    }
+
+    // When guess is too high
   } else if (guess > secretNumber) {
     if(score > 1) {
       document.querySelector('.message').textContent = '📈 Too high!';
@@ -54,4 +60,13 @@ document.querySelector('.check').addEventListener('click', function() {
   }
 });
 
-
+document.querySelector('.again').addEventListener('click', function(){
+  score = 20;
+  secretNumber = Math.trunc(Math.random()*20)+1;
+  document.querySelector('.message').textContent = 'Start guessing...';
+  document.querySelector('.score').textContent = score;
+  document.querySelector('.number').textContent = '?';
+  document.querySelector('.guess').value = '';
+  document.querySelector('body').style.backgroundColor = '#222';
+  document.querySelector('.number').style.width = '15rem';
+});
