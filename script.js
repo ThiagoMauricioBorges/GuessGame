fbf26be066cdf07763bdb4d4407fb7d910c4c52d
Thiago Mauricio Borges <tmb050703@gmail.com>
2025-02-19 16:32:05 -0300

    Media Queries

--- script.js
+++ script.js
@@ -1,19 +1,9 @@
 'use strict';
 
-// console.log(document.querySelector('.message').textContent);
-// document.querySelector('.message').textContent = '🎉 Correct Number! ';
-
-// document.querySelector('.number').textContent = 13;
-// document.querySelector('.score').textContent = 10;
-
-// document.querySelector('.guess').value = 23;
-// console.log(document.querySelector('.guess').value);
-
-
-
 let secretNumber = Math.trunc(Math.random()*20)+1;
 let score = 20;
 let highscore = 0;
+// Show answer
 // document.querySelector('.number').textContent = secretNumber;
 
 document.querySelector('.check').addEventListener('click', function() {
@@ -29,7 +19,7 @@ document.querySelector('.check').addEventListener('click', function() {
     document.querySelector('.message').textContent = '🎉 Correct Number! ';
     document.querySelector('.number').textContent = secretNumber;
     document.querySelector('body').style.backgroundColor = '#60b347';
-    document.querySelector('.number').style.width = '30rem';
+    // document.querySelector('.number').style.width = '15rem';
 
     if(score > highscore) {
       highscore = score;
@@ -67,6 +57,6 @@ document.querySelector('.again').addEventListener('click', function(){
   document.querySelector('.score').textContent = score;
   document.querySelector('.number').textContent = '?';
   document.querySelector('.guess').value = '';
-  document.querySelector('body').style.backgroundColor = '#222';
-  document.querySelector('.number').style.width = '15rem';
+  document.querySelector('body').style.backgroundColor = '#F2F2F2';
+  // document.querySelector('.number').style.width = '15rem';
 });
